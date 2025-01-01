fbc8dd75f48d4ad13713ee6d8bd5c3b9bbf149d2
Krishnendhu Arun <kforkrishnendhu@gmail.com>
2025-01-01 13:26:20 +0530

    first commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+
+@Input() message :string='';
+@Output() messageEvent=new EventEmitter<string>();
+
+sendMessage() {
+  this.messageEvent.emit('Hello from child');
+  }
+}
